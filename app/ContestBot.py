7cc0e5a04599adbebad1629b91940db0304a5b65
Nathan Cheval <nathan.cheval@outlook.fr>
2018-05-23 14:09:11 +0200

    add log

--- app/ContestBot.py
+++ app/ContestBot.py
@@ -31,46 +31,57 @@ LOGGER.setLevel(logging.DEBUG)
 
 
 def follow_user():
-    user = FollowQueue.get()[0]
-    screen_name = user[0]
-    text = user[1]
-    api.create_friendship(screen_name)  # Follow tweet author
-    LOGGER.info('Follow author :  @' + screen_name)
-    if '@' in text:  # Find if we need to follow other user (in addition to the author)
-        usersToFollow = re.findall('@\S+', text)
-        for user in usersToFollow:
-            try:
-                api.create_friendship(user)
-                LOGGER.info('Follow user : ' + user)
-            except tweepy.error.TweepError as tweepError:
-                LOGGER.error(str(tweepError).encode('utf-8'))
-                break
+    if FollowQueue.empty() is not True:
+        user = FollowQueue.get()[0]
+        screen_name = user[0]
+        text = user[1]
+        api.create_friendship(screen_name)  # Follow tweet author
+        LOGGER.info('Follow author :  @' + screen_name)
+        if '@' in text:  # Find if we need to follow other user (in addition to the author)
+            usersToFollow = re.findall('@\S+', text)
+            for user in usersToFollow:
+                try:
+                    api.create_friendship(user)
+                    LOGGER.info('Follow user : ' + user)
+                except tweepy.error.TweepError as tweepError:
+                    LOGGER.error(str(tweepError).encode('utf-8'))
+                    break
+    else:
+        LOGGER.error('Follow queue empty')
 
 def retweet():
-    RT_id = RTQueue.get()
-    try:
-        api.retweet(RT_id)
-    except tweepy.error.TweepError as tweepError:  # Workaround
-        LOGGER.error(str(tweepError).encode('utf-8'))  # Because the key Retweeted
-        pass  # isn't work and always set as False, so we can never check if the tweet already retweeted
-
+    if RTQueue.empty() is not True:
+        RT_id = RTQueue.get()
+        try:
+            api.retweet(RT_id)
+            LOGGER.info('RT tweet ')
+        except tweepy.error.TweepError as tweepError:  # Workaround
+            LOGGER.error(str(tweepError).encode('utf-8'))  # Because the key Retweeted
+            pass  # isn't work and always set as False, so we can never check if the tweet already retweeted
+    else:
+        LOGGER.error('RT queue empty')
 
 def update_status():
-    updated_tweet = TweetQueue.get()
-    try:
-        api.update_status(updated_tweet)
-        LOGGER.info('Tweet random stuff')
-    except tweepy.error.TweepError as tweepError:
-        LOGGER.error(str(tweepError).encode('utf-8'))
-        pass
+    if TweetQueue.empty() is not True:
+        updated_tweet = TweetQueue.get()
+        try:
+            api.update_status(updated_tweet)
+            LOGGER.info('Tweet random stuff')
+        except tweepy.error.TweepError as tweepError:
+            LOGGER.error(str(tweepError).encode('utf-8'))
+            pass
+    else:
+        LOGGER.error('Tweet queue empty')
 
 
 def process_queue():
+    LOGGER.info("RT size : " + str(RTQueue.qsize()) + ' - Follow size : ' + str(FollowQueue.qsize()) + ' - Tweet size : ' + str(TweetQueue.qsize()))
     queue_list = [retweet, follow_user, update_status]
-    while RTQueue.empty() is not True and FollowQueue.empty() is not True and TweetQueue.empty is not True:  # Empty the Queues randomly
+    while RTQueue.empty() is not True or FollowQueue.empty() is not True or TweetQueue.empty() is not True:  # Empty the Queues randomly
         choice(queue_list)()
-        LOGGER.info("Let's go slepping for a while")
-        sleep(randint(100, 500))  # Wait in order to don't spam too much
+        time = randint(100,500)
+        LOGGER.info("Let's go slepping for " + str(time/60) + " min")
+        sleep(time)  # Wait in order to don't spam too much
 
 if __name__ == '__main__':
     # OAuth to Twitter and get Api object
@@ -85,38 +96,42 @@ if __name__ == '__main__':
     # Start contest spamming process
     while True:
         for keyword in search_keyword:
-            tweetsList = tweepy.Cursor(
-                api.search,
-                q=keyword,  # search query is case insensitive
-                lang=lang,
-                tweet_mode="extended",
-                result_type="recent",
-            ).items(int(nb_tweets))
+            try:
+                tweetsList = tweepy.Cursor(
+                    api.search,
+                    q=keyword,  # search query is case insensitive
+                    lang=lang,
+                    tweet_mode="extended",
+                    result_type="recent",
+                ).items(int(nb_tweets))
 
-            for tweet in tweetsList:
-                isRT = hasattr(tweet, 'retweeted_status')  # Check if it's a RT
-                if not tweet.is_quote_status and not tweet.in_reply_to_status_id and not any(word for word in banned_words if word in tweet.full_text.lower()):  # Do net process quoted status and response of a tweet
-                    if not tweet.retweeted:  # Check you didn't already RT this tweet (not working for now)
-                        if isRT:
-                            rt = tweet.retweeted_status.id
-                        else:
-                            rt = tweet.id
-                        RTQueue.put(rt)
+                for tweet in tweetsList:
+                    isRT = hasattr(tweet, 'retweeted_status')  # Check if it's a RT
+                    if not isRT and not tweet.is_quote_status and not tweet.in_reply_to_status_id and not any(word for word in banned_words if word in tweet.full_text.lower()):  # Do net process quoted status and response of a tweet
+                        if not tweet.retweeted:  # Check you didn't already RT this tweet (not working for now)
+                            if isRT:
+                                rt = tweet.retweeted_status.id
+                            else:
+                                rt = tweet.id
+                            RTQueue.put(rt)
 
-                        # Search follow keyword
-                        follow = None
-                        if isRT:
-                            if any(keyword in tweet.retweeted_status.full_text for keyword in follow_keyword):
-                                follow = [tweet.retweeted_status.user.screen_name, tweet.retweeted_status.full_text]
-                        else:
-                            if any(keyword in tweet.full_text for keyword in follow_keyword):
-                                follow = [tweet.user.screen_name, tweet.full_text]
-                        if follow is not None:
-                            FollowQueue.put([(follow[0], follow[1])])
+                            # Search follow keyword
+                            follow = None
+                            if isRT:
+                                if any(keyword in tweet.retweeted_status.full_text for keyword in follow_keyword):
+                                    follow = [tweet.retweeted_status.user.screen_name, tweet.retweeted_status.full_text]
+                            else:
+                                if any(keyword in tweet.full_text for keyword in follow_keyword):
+                                    follow = [tweet.user.screen_name, tweet.full_text]
+                            if follow is not None:
+                                FollowQueue.put([(follow[0], follow[1])])
 
-                        # Tweet Random quotes
-                        for citation in urlopen('https://talaikis.com/api/quotes/random/'):
-                            #randomTweet = json.loads(citation.decode('utf-8'))['citation']['citation']
-                            randomTweet = json.loads(citation.decode('utf-8'))['quote']
-                        TweetQueue.put(randomTweet)
-            process_queue()
+                            # Tweet Random quotes
+                            for citation in urlopen('https://talaikis.com/api/quotes/random/'):
+                                randomTweet = json.loads(citation.decode('utf-8'))['quote']
+                            TweetQueue.put(randomTweet)
+                process_queue()
+            except tweepy.error.TweepError as tweepError:
+                LOGGER.error(str(tweepError).encode('utf-8'))
+                pass
+
